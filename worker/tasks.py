d98003c4cf78a6c5db21aa1b1ef1e3b184f42313
Diego Rodriguez <diego.rodriguez.rodriguez@cern.ch>
2016-09-29 14:22:32 +0200

    celery: fibonacci function improvement
    * Improves fibonacci impl from recursion to generator based.

--- worker/tasks.py
+++ worker/tasks.py
@@ -3,11 +3,16 @@ from __future__ import absolute_import
 from worker.celery import app
 
 
-def fibo(number):
-    return number if number < 2 else \
-        fibonacci(number-1) + fibonacci(number-2)
+# https://www.python.org/dev/peps/pep-0255/
+def fib():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
 
 
 @app.task(name='tasks.fibonacci')
 def fibonacci(number):
-    return fibo(number)
+    for index, fibonacci_number in enumerate(fib()):
+        if index == number:
+            fibonacci_number
