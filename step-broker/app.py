41973b088aa2cd0c5b6952fcc3cac9f9c13231c4
Diego Rodriguez <diego.rodriguez.rodriguez@cern.ch>
2016-10-24 17:50:48 +0200

    step-broker: WIP status data storage

--- step-broker/app.py
+++ step-broker/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, abort, jsonify, request
+import threading
 import pykube
 
 app = Flask(__name__)
 app.secret_key = "mega secret key"
 
-jobs = []
-
 api = pykube.HTTPClient(pykube.KubeConfig.from_service_account())
-
+job_db = {}
 
 @app.route('/api/v1.0/jobs', methods=['GET'])
 def get_jobs():
@@ -70,10 +69,30 @@ def create_job():
     }
 
     pykube.Job(api, job).create()
-
+    job_db[request.json['job-name']] = 'started'
     return jsonify({'job': job}), 201
 
 
+@app.route('/api/v1.0/jobs/watch/<job_id>', methods=['GET'])
+def get_job(job_id):
+    if job_id in job_db:
+        return jsonify({'job': job_db[job_id]}), 200
+    else:
+        abort(404)
+
+
+def watch_jobs():
+    while True:
+        stream = pykube.Job.objects(api).filter(namespace=pykube.all).watch()
+        for line in stream:
+            job = line[1].obj
+            if job['metadata']['name'] in job_db:
+                job_db[job['metadata']['name']] = job['status']
+                print(job_db[job['metadata']['name']])
+
+
 if __name__ == '__main__':
+    events_thread = threading.Thread(target=watch_jobs)
+    events_thread.start()
     app.run(debug=True, port=5000,
             host='0.0.0.0')
